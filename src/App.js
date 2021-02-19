e17c513bf4fb9f7d1234e03e50181333fa0ec208
Franci G <franci@handels.com>
2021-02-18 21:23:31 -0800

    created Card component & class

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
+import React from "react";
 import "./App.css";
+import Card from "./Card";
 
 //Component to rep a single card
 //Another component to rep a list
 
-function App() {
-  return (
-    <div className="App">
-      <h1> The GitHub Cards App</h1>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="header">
+        <h1>The Github Cards App</h1>
+        <Card />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Card.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Card extends React.Component {
+  render() {
+    return <div className="github-profile">One GitHub Profile... </div>;
+  }
+}
+
+export default Card;
